8abd54252e3b6ff129b2c4f1dc979a7b1e1d132b
AdreanRodriguez <loco_bladare@hotmail.com>
2024-01-11 15:01:05 +0100

    first commit

---
+++ min-egna/script.js
@@ -0,0 +1,23 @@
+// 1. Skapa ett objekt som innehåller data om en bok,
+// nycklar som kan inkluderas är `title`, `author`, `numPages`.
+
+let book = [
+  {
+    title: "Teenage mutant ninja turtles",
+    author: "Shredder",
+    numPages: 578
+  }
+]
+
+// 2. Skapa en array av objekt med flera stycken böcker
+
+let books = [
+  {title: `TMNT`, author: `Kevin Eastman`, numPages: 2387},
+  {title: `HGTTG`, author: `Douglad Adams`, numPages: 2278},
+  {title: `The jungel book`, author: `Rudyard Kipling`, numPages: 231 }
+]
+
+// 3. Skapa en funktion som söker efter en titel i arrayen,
+// om den hittar en så returneras objektet annars `null`.
+// Låt funktionen ta emot arrayen med bok-objekt och söktermen som parametrar.
+
